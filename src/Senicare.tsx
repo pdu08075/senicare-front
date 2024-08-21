81ae77f8cab0785815b11049095faf4462dc5682
pdu08075 <pdu08075>
2024-08-21 12:51:06 +0900

    2024.08.21.12

--- src/Senicare.tsx
+++ src/Senicare.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Senicare.css';
+import Auth from 'src/view/Auth';
 
 export default function Senicare() {
   return (
     <>
+      <Auth />
     </>
   );
 }
