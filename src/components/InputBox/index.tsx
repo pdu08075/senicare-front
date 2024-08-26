a90454ce35aeef29592fc909ceb10b9d26c809ef
pdu08075 <pdu08075>
2024-08-26 10:51:59 +0900

    2024.08.26.10

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import './style.css';
 
 interface Props {
@@ -7,15 +7,26 @@ interface Props {
     placeholder: string;
     value: string;
     buttonName? : string;
+
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onButtonClick?: () => void;
 }
 
-export default function InputBox({ label, type, placeholder, value, buttonName} : Props) {
+export default function InputBox({
+    label,
+    type,
+    placeholder,
+    value,
+    buttonName,
+    onChange,
+    onButtonClick,
+} : Props) {
     return (
         <div className="input-box">
             <div className="label">{label}</div>
             <div className="input-area">
-                <input value={value} type={type} placeholder={placeholder} onChange={} />
-                {buttonName && <div className="input-button disable">{buttonName}</div>}
+                <input value={value} type={type} placeholder={placeholder} onChange={onChange} />
+                {buttonName && <div className={`input-button ${value ? 'active' : 'disable'}`} onClick={onButtonClick} >{buttonName}</div>}
             </div>
             <div className="message"></div>
         </div>
