7b7caf342a11015e484da4e309e8fbc50019ac3a
pdu08075 <pdu08075>
2024-09-04 11:53:59 +0900

    2024.09.04.11

--- src/Senicare.tsx
+++ src/Senicare.tsx
@@ -1,24 +1,49 @@
 import React, { useEffect } from 'react';
 import './Senicare.css';
 import Auth from 'src/view/Auth';
-import { Route, Routes } from 'react-router';
+import { Route, Routes, useNavigate } from 'react-router';
+import MainLayout from './layouts/MainLayout';
 
-function index() {
+// component: root path 컴포넌트//
+function Index() {
 
+  // function: 네비게이터 함수//
+  const navigator = useNavigate();
+
+  //effect: 마운트 시 경로 이동 effect //
   useEffect(() => {
-    // TODO: /auth로 경로 이동
+    navigator('/auth'); // TODO: /auth로 경로 이동
   }, []);   // 배열 생략 가능
+
+  // render: root path 컴포넌트 렌더링//
 
   return (
     <></>
-  )
+  );
 }
 
+// component: Senicare 컴포넌트//
 export default function Senicare() {
+  
+    // render: root path 컴포넌트 렌더링//
   return (
     <Routes>
-      <Route index element={<> </>} />    {/* index가 기본 경로 (ex: http://localhost:3000) */}
+      <Route index element={<Index />} />    {/* index가 기본 경로 (ex: http://localhost:3000) */}
       <Route path='/auth' element={<Auth />} />
+      <Route path='/cs' element={<MainLayout />}>
+        <Route index element={<>고객 리스트 보기</>} />
+        <Route path='write' element={<>고객 등록</>} />
+        <Route path=':customNumber' element={<>고객 정보 보기</>} />
+        <Route path=':customNumber/update' element={<>고객 정보 수정</>} />
+      </Route>
+      <Route path='/mm' element={<MainLayout />}>
+        <Route index element={<></>} />
+      </Route>
+      <Route path='/hr' element={<MainLayout />}>
+        <Route index element={<></>} />
+        <Route path=':userId' element={<></>} />
+        <Route path=':userId/update' element={<></>} />      
+      </Route>
     </Routes>
   );
 }
