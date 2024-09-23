b015727d08f20509e4e56eb8faf9e6f042125f2f
pdu08075 <rpaeheh@naver.com>
2024-09-23 13:49:35 +0900

    2024.09.23.12

--- src/view/MM/index.tsx
+++ src/view/MM/index.tsx
@@ -5,6 +5,8 @@ import { ACCESS_TOKEN } from 'src/constants';
 import { PostToolRequestDto } from 'src/apis/dto/request/tool';
 import { getToolListRequest, postToolRequest } from 'src/apis';
 import { ResponseDto } from 'src/apis/dto/response';
+import { Tool } from 'src/types';
+import { GetToolListResponseDto } from 'src/apis/dto/response/tool';
 
 // interface: 용품 등록 컴포넌트 Properties //
 interface PostBoxProps {
@@ -128,6 +130,41 @@ function PatchBox() {
 
 }
 
+
+// interface: 용품 리스트 아이템 컴포넌트 Properties //
+interface TableRowProps {
+    tool: Tool,
+}
+
+// component: 용품 리스트 아이템 컴포넌트 //
+function TableRow({ tool }: TableRowProps) {
+
+    // render: 용품 리스트 아이템 컴포넌트 렌더링 //
+    return (
+        <div className='tr'>
+            <div className='td-tool-number'>{tool.toolNumber}</div>
+            <div className='td-name'>{tool.name}</div>
+            <div className='td-purpose'>{tool.purpose}</div>
+            <div className='td-count'>{tool.count}</div>
+            <div className='td-buttons'>
+                <div className='td-edit'>
+                    <div className='icon-button edit'></div>
+                </div>
+                <div className='td-delete'>
+                    <div className='icon-button trash'></div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+// variable: 페이지 당 아이템 수 //
+const ITEMS_PER_PAGE = 5;
+
+// variable: 페이지 당 아이템 수 //
+const PAGES_PER_SECTION = 5;
+
 // component: 용품 관리 리스트 컴포넌트 //
 export default function MM() {
 
@@ -138,6 +175,35 @@ export default function MM() {
     const [showPostBox, setShowPostBox] = useState<boolean>(false);
     const [showPatchBox, setShowPatchBox] = useState<boolean>(false);
 
+    // state: 용품 리스트 상태 //
+    const [toolList, setToolList] = useState<Tool[]>([]);
+
+    //state: 페이지 처리 //
+    const [totalCount, setTotalCount] = useState<number>(0);
+    const [totalPage, setTotalPage] = useState<number>(0);
+    const [totalSection, setTotalSection] = useState<number>(0);
+    const [currentPage, setCurrentPage] = useState<number>(0);
+    const [currentSection, setCurrentSection] = useState<number>(0);
+    const [pageList, setPageList] = useState<number[]>([]);
+
+
+    // function: get tool list response 처리 함수 //
+    const getToolListResponse = (resposenBody: GetToolListResponseDto | ResponseDto | null) => {
+        const message = 
+            !resposenBody ? '서버에 문제가 있습니다.' :
+            resposenBody.code === 'AF' ? '잘못된 접근입니다.' :
+            resposenBody.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
+
+        const isSuccessed = resposenBody !== null && resposenBody.code === 'SU';
+        if (!isSuccessed) {
+            alert(message);
+            return;
+        }
+
+        const { tools } = resposenBody as GetToolListResponseDto;
+        setToolList(tools);
+    };
+
     // function: 등록 박스 뷰 상태 변경 함수 //
     const unShowPostBox = () => setShowPostBox(false);
 
@@ -147,12 +213,59 @@ export default function MM() {
         setShowPatchBox(false);
     }
 
+    // event handler: 페이지 클릭 이벤트 처리 함수 //
+    const onPageClickHandler = (page: number) => {
+        setCurrentPage(page);
+    };
+
+    // event handler: 이전 섹션 클릭 이벤트 처리 함수 //
+    const onPreSectionClickHandler = () => {
+        if (currentSection === 1) return;
+        setCurrentSection(currentSection - 1);
+        setCurrentPage((currentSection - 1) * PAGES_PER_SECTION);
+    };
+
+    // event handler: 다음 섹션 클릭 이벤트 처리 함수 //
+    const onNextSectionClickHandler = () => {
+        if (currentSection === totalSection) return;
+        setCurrentSection(currentSection + 1);
+        setCurrentPage(currentSection * PAGES_PER_SECTION + 1);
+    };
+
     // effect: 컴포넌트 로드 시 용품 리스트 불러오기 함수 //
     useEffect(() => {
         const accessToken = cookies[ACCESS_TOKEN];
         if (!accessToken) return;
-        getToolListRequest(accessToken).then();
+        getToolListRequest(accessToken).then(getToolListResponse);
     }, []);
+
+    // effect: toolList가 변경될 시 실행할 함수 //
+    useEffect(() => {
+        if (!toolList.length) return;
+        const totalCount = toolList.length;
+        setTotalCount(totalCount);
+        const totalPage = Math.ceil(totalCount / ITEMS_PER_PAGE);
+        setTotalPage(totalPage);
+        const totalSection = Math.ceil(totalPage / PAGES_PER_SECTION);
+        setTotalSection(totalSection);
+
+        setCurrentPage(1);
+        setCurrentSection(1);
+    }, [toolList]);
+
+    // effect: 현재 섹션이 변경될 시 실행할 함수 //
+    useEffect(() => {
+        const startPage = PAGES_PER_SECTION * currentSection - (PAGES_PER_SECTION - 1);
+        let endPage = PAGES_PER_SECTION * currentSection;
+        if (endPage > totalPage) endPage = totalPage;
+
+        const pageList = [];
+        for (let page = startPage; page <= endPage; page++) {
+            pageList.push(page);
+        }
+        setPageList(pageList);
+
+    }, [currentSection]);
 
     // render: 용품 관리 리스트 컴포넌트 렌더링 //
     return (
@@ -160,7 +273,7 @@ export default function MM() {
             {showPostBox && <PostBox unShow={unShowPostBox} />}
             {showPatchBox && <PatchBox />}
             <div className='top'>
-                <div className='top-text'>전체 <span className='emphasis'>150건</span> | 페이지 <span className='emphasis'>1/100</span></div>
+                <div className='top-text'>전체 <span className='emphasis'>{totalCount}건</span> | 페이지 <span className='emphasis'>{currentPage}/{totalPage}</span></div>
                 {!showPostBox && !showPatchBox && <div className='button primary' onClick={onPostButtonClickHandler}>등록</div>}
             </div>
             <div className='main'>
@@ -174,23 +287,24 @@ export default function MM() {
                             <div className='td-edit'>수정</div>
                             <div className='td-delete'>삭제</div>
                         </div>
-                        
                     </div>
-                    <div className='tr'>
-                    <div className='td-tool-number'>용품번호</div>
-                        <div className='td-name'>용품명</div>
-                        <div className='td-purpose'>용도</div>
-                        <div className='td-count'>개수</div>
-                        <div className='td-buttons'>
-                            <div className='td-edit'>수정</div>
-                            <div className='td-delete'>삭제</div>
-                        </div>
-                    </div>
+                    {toolList.map((tool, index) => <TableRow key={index} tool={tool} />)}
                 </div>
-                
             </div>
-            
-            <div className='bottom'></div>
+            <div className='bottom'>
+                <div className='pagination-box'>
+                    <div className='round-left-button' onClick={onPreSectionClickHandler}></div>
+                    <div className='page-list'>
+                        {pageList.map(page => <div key={page} className={page === currentPage ? 'page active' : 'page'} onClick={() => onPageClickHandler(page)}>{page}</div>)}
+                    </div>
+                    <div className='round-right-button' onClick={onNextSectionClickHandler}></div>
+                </div>
+                <div className='search-box'>
+                    <input className='search-input' placeholder='검색어를 입력하세요.' />
+                    <div className='button disable'>검색</div>
+                </div>
+
+            </div>
         </div>
     )
 }
