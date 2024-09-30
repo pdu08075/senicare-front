88920fae41fb3ec321cade0307ff0633e3e92ffb
pdu08075 <rpaeheh@naver.com>
2024-09-30 13:03:04 +0900

    2024.09.30.12

---
+++ src/apis/dto/request/customer/post-care-record-request.dto.ts
@@ -0,0 +1,7 @@
+// interface: posr care record request body dto //
+export default interface PostCareRecordRequestDto {
+    contents: string;
+    usedToolNumber: number | null;
+    count: number | null;
+
+}
